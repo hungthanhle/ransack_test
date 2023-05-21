658b193db7001031c026f83baa1ac842e90d923c
hung <hunglt.ee@gmail.com>
2023-05-21 22:27:30 +0700

    ransack query

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,14 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.ransack(name_cont: "but").result
-    # @products = Product.ransack(name_eq: "but").result
+    # http://localhost:3000/products?q=but&page=1 <- SAI
+    # http://localhost:3000/products?q%5Bname_cont%5D=but&commit=Search
+    # http://localhost:3000/products?q[name_cont]=but&commit=Search&page=1
+    @q = Product.ransack(params[:q])
+    # @products = @q.result
+    @products = @q.result.page(params[:page]).per(1)
+    if !params[:q].nil?
+      render json: @products
+    end
   end
 end
