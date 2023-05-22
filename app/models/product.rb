6c17880a981650bd6c6007fbe26e5aa2cda48a9f
hung <hunglt.ee@gmail.com>
2023-05-22 08:33:30 +0700

    ransack mutiple field query

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ApplicationRecord
-  def self.ransackable_attributes(auth_object = nil)
-    ["content", "created_at", "description", "id", "name", "price", "updated_at"]
+
+  class << self
+    def ransackable_attributes(auth_object = nil)
+      ["content", "created_at", "description", "id", "name", "price", "updated_at"]
+    end
+
+    def ransackable_associations(auth_object = nil)
+      []
+    end
   end
 end
